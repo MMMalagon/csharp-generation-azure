b0d026a649f5221866a4474031430350434e3eb1
MaanuelMM <45206520+MaanuelMM@users.noreply.github.com>
2023-03-31 22:13:54 +0200

    Add Northwind DB model using scaffolding

---
+++ EntityFrameworkDemo/Model/Products_Above_Average_Price.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkDemo.Model;
+
+public partial class Products_Above_Average_Price
+{
+    public string ProductName { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+}
