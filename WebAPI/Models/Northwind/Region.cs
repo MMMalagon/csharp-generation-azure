e2e6eb7ad30b3ba5dbcf8102592d4d550ec69834
MaanuelMM <45206520+MaanuelMM@users.noreply.github.com>
2023-04-11 12:43:28 +0200

    Add WebAPI project as a RESTful API example

---
+++ WebAPI/Models/Northwind/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models.Northwind;
+
+public partial class Region
+{
+    public int RegionID { get; set; }
+
+    public string RegionDescription { get; set; } = null!;
+
+    public virtual ICollection<Territory> Territories { get; } = new List<Territory>();
+}
---
+++ WebAPI/Models/Northwind/Shipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models.Northwind;
+
+public partial class Shipper
+{
+    public int ShipperID { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public virtual ICollection<Order> Orders { get; } = new List<Order>();
+}
